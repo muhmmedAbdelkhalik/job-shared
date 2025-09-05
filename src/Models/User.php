5f01d2dd490605b32574582da527c2aa063bfe1e
M.Abdelkhalik <muhmmedabdelkhalik@gmail.com>
2025-09-06 02:32:03 +0300

    Enhance User model to support API tokens
    - Added HasApiTokens trait to the User model to enable API token authentication.

--- src/Models/User.php
+++ src/Models/User.php
@@ -8,11 +8,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasUuids, SoftDeletes;
+    use HasFactory, Notifiable, HasUuids, SoftDeletes, HasApiTokens;
 
     protected $keyType = 'string';
     public $incrementing = false;
